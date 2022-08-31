338fdf39134afc64ebf2bda4538ce6d9d6a2814b
Sosuke1019 <kato.052wazime@gmail.com>
2022-08-31 20:43:47 +0900

    プログラム実行時の修正

--- mini_board/app.js
+++ mini_board/app.js
@@ -5,13 +5,13 @@ const url = require('url');
 const qs = require('querystring');
 
 
-const index_page = fs.readFileSync('./index.ejs','utf-8');
-const login_page = fs.readFileSync('./login.ejs', 'utf-8');
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const login_page = fs.readFileSync('./login.ejs', 'utf8');
 
 
-const max_num = 10; //最大保管数
-const filename = 'mydata.txt'; //データファイル名
-var message_data; //データ
+const max_num = 10; // 最大保管数
+const filename = 'mydata.txt'; // データファイル名
+var message_data; // データ
 readFromFile(filename);
 
 
@@ -25,18 +25,18 @@ console.log('Server start!');
 //ここまでメインプログラム===========================
 
 
-//createServerの処理
-function getFromClient(req,res) {
-    var url_parts = url.parse(req.url, ture);
-    switch (url.parts.pathname) {
-        case'/': //トップページ(メッセージボード)
-            res_index(req,res);
+// createServerの処理
+function getFromClient(req, res) {
+    var url_parts = url.parse(req.url, true);
+    switch (url_parts.pathname) {
+        case '/': // トップページ（メッセージボード）
+            res_index(req, res);
             break;
-        case'/login': //ログインページ
-            res_login(req,res);
+        case '/login': // ログインページ
+            res_login(req, res);
             break;
         default:
-            res.writeHead(200, { 'Content-Type' : 'text/plain '});
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end('no page...');
             break;
     }
@@ -47,7 +47,7 @@ function getFromClient(req,res) {
 function res_login(req,res) {
     var content = ejs.render(login_page, {});
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.writeHead(content);
+    res.write(content);
     res.end();
 }
 
@@ -76,17 +76,17 @@ function res_index(req,res) {
 
 
 //indexのページ作成
-function write_index(req,res) {
-    var msg = "※何かメッセージを書いてください。"
+function write_index(req, res) {
+    var msg = "※何かメッセージを書いて下さい。";
     var content = ejs.render(index_page, {
         title: 'Index',
         content: msg,
         data: message_data,
         filename: 'data_item',
     });
-    res.writeHead(200, { 'Content-Type' : 'text/html' });
-    res.write(content);
-    res.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(content);
+        res.end();
 }
 
 
@@ -98,7 +98,7 @@ function readFromFile(fname) {
 }
 
 //データを更新
-function addToData(id, msg, fname, req); {
+function addToData(id, msg, fname, req) {
     var obj = {'id': id, 'msg':msg};
     var obj_str = JSON.stringify(obj);
     console.log('add data: ' + obj.str);
@@ -112,9 +112,9 @@ function addToData(id, msg, fname, req); {
 
 //データを保存
 function saveToFilename(fname) {
-    var data_str = message_data.json('\n');
+    var data_str = message_data.join('\n');
     fs.writeFile(fname, data_str, (err) => {
-        if (err) { throw err;}
+        if (err) { throw err; }
     });
 }
 
