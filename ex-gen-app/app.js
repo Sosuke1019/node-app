b0c02713cb18061decf28f576c6cc98111cf2ca8
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-04 10:57:14 +0900

    p202Webページを追加してみる～p210フォームの送信について

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hello = require('./routes/hello');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //テンプレートファイルが保管される場所
+app.set('view engine', 'ejs'); //テンプレートエンジンの種類
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//アクセスのためのapp.use
+app.use('/', indexRouter); 
+app.use('/users', usersRouter); 
+app.use('/hello', hello);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
