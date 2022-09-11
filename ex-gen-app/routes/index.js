ff4f4f7a83b9dcaae2ebb52bf5cfe79c67dfd101
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-11 17:02:56 +0900

    fix index.js

--- ex-gen-app/routes/index.js
+++ ex-gen-app/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) { //route.get(アドレス,関数);
+router.get('/', (req, res, next) => { //route.get(アドレス,関数);
   res.render('index', { title: 'Express' });
 });
 
