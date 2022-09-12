5041efc4aafaab3a414b25c97acde996b84daac2
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-12 16:05:55 +0900

    p221 Googleニュースの表示でRSSの取得を理解

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const http = require('https'); 
+const parseString = require('xml2js').parseString;
 
 router.get('/', (req, res, next) => {
-    var msg = '※何か書いて送信してください。';
-    if (req.session.message != undefined) {
-        msg = "Last Message: " + req.session.message;
-    }
-    var data = {
-    title: 'Hello!',
-    content: msg
+    var opt = {
+        host: 'news.google.com',
+        port: 443,
+        path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
     };
-    res.render('hello', data);
-});
-
-router.post('/post', (req,res,next) => {
-    var msg = req.body['message'];
-    req.session.message = msg;
-    var data = {
-        title: 'Hello!',
-        content: 'Last Message:' + req.session.message
-    };
-    res.render('hello',data);
+    //コールバック関数にデータを取得した際のイベントを設定して、データを取得出来るようにする
+    http.get(opt, (res2) => {
+        var body = '';
+        res2.on('data', (data) => {
+            body += data;
+    });
+    //データ受信完了後の処理
+    res2.on('end', () => {
+        parseString(body.trim(), (err, result) => {
+            console.log(result);
+            var data = {
+            title: 'Google News',
+            content: result.rss.channel[0].item
+        };
+            res.render('hello', data);
+            });
+        })
+    });
 });
 
 module.exports = router;
