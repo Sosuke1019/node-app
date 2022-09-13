d0d70a40b54ac102acf6c61b6d64298e0b410c53
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-13 11:38:03 +0900

    p248 データベースのデータを表示する

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const http = require('https'); 
-const parseString = require('xml2js').parseString;
+const sqlite3 = require('sqlite3'); // 追加
 
-router.get('/', (req, res, next) => {
-    var opt = {
-        host: 'news.google.com',
-        port: 443,
-        path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
-    };
-    //コールバック関数にデータを取得した際のイベントを設定して、データを取得出来るようにする
-    http.get(opt, (res2) => {
-        var body = '';
-        res2.on('data', (data) => {
-            body += data;
-    });
-    //データ受信完了後の処理
-    res2.on('end', () => {
-        parseString(body.trim(), (err, result) => {
-            console.log(result);
+// データベースオブジェクトの取得
+const db = new sqlite3.Database('mydb.sqlite3');
+
+// GETアクセスの処理
+router.get('/',(req, res, next) => {
+    // データベースのシリアライズ
+    db.serialize(() => {
+      //レコードをすべて取り出す
+      db.all("select * from mydata",(err, rows) => {
+        // データベースアクセス完了時の処理
+        if (!err) {
             var data = {
-            title: 'Google News',
-            content: result.rss.channel[0].item
-        };
+                title: 'Hello!',
+                content: rows // 取得したレコードデータ
+            };
             res.render('hello', data);
-            });
-        })
-    });
+            }
+        }); 
+    }); 
 });
 
 module.exports = router;
