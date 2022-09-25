7138764674027b89a18b7176eddd36c136090b30
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-25 15:02:11 +0900

    section6-2 レコードを検索する

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+const Op = db.Sequelize.Op
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+///usersにアクセスした際の処理
+router.get('/',(req, res, next)=> {
+  const nm = req.query.name;
+  const ml = req.query.mail;
+  db.User.findAll({
+    where: {
+      [Op.or]:[
+        {name:{[Op.like]:'%'+nm+'%'}},
+        {mail:{[Op.like]:'%'+ml+'%'}},
+      ]
+    }
+  }).then(usrs => {
+    var data = {
+      title: 'Users/Index',
+      content: usrs
+    }
+    res.render('users/index', data);
+  });
 });
 
 module.exports = router;
