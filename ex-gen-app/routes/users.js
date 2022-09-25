ee5f57603c6ecdc57845bbefeea6ef8f8edbddb5
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-26 01:39:38 +0900

    p360 Userの作成処理をバリデーションに対応する

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -20,24 +20,38 @@ router.get('/',(req, res, next)=> {
 
 router.get('/add',(req, res, next)=> {
   var data = {
-    title: 'Users/Add'
+    title: 'Users/Add',
+    form: new db.User(),
+    err: null
   }
   res.render('users/add', data);
 });
 
 router.post('/add',(req, res, next)=> {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  };
   db.sequelize.sync()
   //{}に保存するレコードの情報が用意される
-    .then(() => db.User.create({
-      name: req.body.name,
-      pass: req.body.pass,
-      mail: req.body.mail,
-      age: req.body.age
-    }))
+    .then(() => db.User.create(form)
     //create実行後の処理
+    //成功した時の処理
     .then(usr => {
       res.redirect('/users');
-    });
+    })
+    //失敗した時の処理
+    .catch(err=> {
+      var data = {
+        title: 'Users/Add',
+        form: form,
+        err: err
+      }
+      res.render('users/add', data);
+    })
+    )
 });
 
 router.get('/edit',(req,res,next) => {
