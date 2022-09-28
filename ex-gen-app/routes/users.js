68c643fce2308a9e97918e22ebcaae2888e49d61
Sosuke1019 <kato.052wazime@gmail.com>
2022-09-28 17:03:43 +0900

    wip ログイン処理をusers.jsに追加

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -96,4 +96,40 @@ router.post('/delete',(req,res,next) =>{
   });
 });
 
+// /loginにアクセスした際の処理
+router.get('/login',(req,res,next) => {
+  var data = {
+    title: 'Users/Login',
+    content: '名前とパスワードを入力下さい'
+  }
+  res.render('users/login',data)
+});
+
+// フォームを送信された際の処理
+router.post('/login',(req,res,next) => {
+  db.User.findOne({
+    where: {
+      name:req.body.name,
+      pass:req.body.pass,
+    }
+  }).then (usr => {
+    if(usr != null) {
+      //セッションのログインという値に取り出したUserオブジェクトを保存
+      res.session.login = usr;
+      let back = req.session.back;
+      if (back == mull) {
+        back = '/';
+      }
+      res.redirect(back);
+    } else {
+      var data = {
+        title: 'Users/Login',
+        content: '名前かパスワードに問題があります。再度入力下さい。'
+      }
+      res.render('users/login', data);
+    }
+  })
+});
+
+
 module.exports = router;
